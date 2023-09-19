88f3b174f0b349daea3029e56345c77623ee83dd
Sheng Chen <sheche@microsoft.com>
2023-09-19 16:32:07 +0800

    docs: Add license headers
    Signed-off-by: Sheng Chen <sheche@microsoft.com>

--- server/src/test/java/com/microsoft/java/bs/core/internal/services/LifecycleServiceTest.java
+++ server/src/test/java/com/microsoft/java/bs/core/internal/services/LifecycleServiceTest.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 package com.microsoft.java.bs.core.internal.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
