a74708c039488398da47abfb6889ae1e64d18c6f
chagong <shawn.gong@hotmail.com>
2025-08-07 15:48:24 +0800

    fix: use apache lang3 for stringUtils (#205)

--- server/src/main/java/com/microsoft/java/bs/core/internal/gradle/Utils.java
+++ server/src/main/java/com/microsoft/java/bs/core/internal/gradle/Utils.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import org.gradle.internal.impldep.org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.gradle.tooling.BuildAction;
 import org.gradle.tooling.BuildActionExecuter;
 import org.gradle.tooling.BuildLauncher;
