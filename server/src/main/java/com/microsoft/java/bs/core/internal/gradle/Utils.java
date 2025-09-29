6a9885f88f66ad9be77bf0ff84d6bd735fe2dbe7
wenyt <75360946+wenytang-ms@users.noreply.github.com>
2025-09-29 13:22:28 +0800

    feat: support gradle 9.1 (#207)
    * feat: support gradle 9.1
    
    * fix: remove unused import
    
    * fix: build error
    
    * ci: update the vm image version
    
    * ci: update pipeline
    
    * ci: update pipeline
    
    * ci: update
    
    * ci: update gradle for ci
    
    * ci: update version
    
    * fix: test case

--- server/src/main/java/com/microsoft/java/bs/core/internal/gradle/Utils.java
+++ server/src/main/java/com/microsoft/java/bs/core/internal/gradle/Utils.java
@@ -154,7 +154,13 @@ public class Utils {
    */
   public static String getLatestCompatibleJavaVersion(String gradleVersion) {
     GradleVersion version = GradleVersion.version(gradleVersion);
-    if (version.compareTo(GradleVersion.version("8.8")) >= 0) {
+    if (version.compareTo(GradleVersion.version("9.1")) >= 0) {
+      return "25";
+    } else if (version.compareTo(GradleVersion.version("8.14")) >= 0) {
+      return "24";
+    } else if (version.compareTo(GradleVersion.version("8.10")) >= 0) {
+      return "23";
+    } else if (version.compareTo(GradleVersion.version("8.8")) >= 0) {
       return "22";
     } else if (version.compareTo(GradleVersion.version("8.5")) >= 0) {
       return "21";
